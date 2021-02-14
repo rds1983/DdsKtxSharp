6940b66a23b2a572b4495b4bc54e62c73a3eee3b
Roman Shapiro <roman.shapiro@grapecity.com>
2021-02-14 15:54:05 +0700

    Initial code commit

---
+++ src/DdsKtxSharp.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DdsKtxSharp
+{
+#if !DDSKTXSHARP_INTERNAL
+	public
+#else
+	internal
+#endif
+	static unsafe partial class DdsKtx
+	{
+		public static string LastError;
+
+		private static unsafe void dds_ktx_err(string error)
+		{
+			ddsktx__dds_translate_pixel_format f = new ddsktx__dds_translate_pixel_format { bit_mask = new uint[] { 1, 2, 3, 4 } };
+
+			LastError = error;
+		}
+	}
+}
